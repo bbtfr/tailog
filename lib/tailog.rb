f7492b7d62f57e194376c63e97bc15529b79a88b
bbtfr <bbtfrr@gmail.com>
2016-04-19 11:32:48 +0800

    add http basic auth for all the requests

--- lib/tailog.rb
+++ lib/tailog.rb
@@ -26,14 +26,21 @@ module Tailog
 
   class << self
     attr_accessor :log_path
+    attr_accessor :basic_auth
   end
 
   self.log_path = File.expand_path("log", Dir.pwd)
+  self.basic_auth = proc do |username, password|
+    username == (ENV['TAILOG_USERNAME'] || 'tailog') and
+      password == (ENV['TAILOG_USERNAME'] || 'password')
+  end
 
   class App < Sinatra::Base
     set :root, File.expand_path("../../app", __FILE__)
     set :public_folder do "#{root}/assets" end
     set :views do "#{root}/views" end
+
+    use Rack::Auth::Basic, "Restricted Area", &Tailog.basic_auth
 
     helpers do
       def h(text)
